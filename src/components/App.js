9cd84eaacac6043e4d28a29733ae4fc083378acb
Prashant Kumar <prashant.aman9@gmail.com>
2024-08-08 13:46:55 +0530

    Complete Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
-import React from "react";
-import './../styles/App.css';
+import React from 'react'
+import './../styles/App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home'
+import About from '../pages/About'
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+	return (
+		<div>
+			{/* Do not remove the main div */}
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/users/:id' element={<About />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	)
 }
 
 export default App
---
+++ src/pages/About.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+function About() {
+	const { id } = useParams()
+	const [user, setUser] = useState({})
+	const [loading, setLoading] = useState(true)
+
+	useEffect(() => {
+		setLoading(true)
+		fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+			.then((response) => response.json())
+			.then((data) => setUser(data))
+			.finally(() => setLoading(false))
+	}, [id])
+
+    if (loading) {
+        return <p>Loading...</p>
+    }
+
+	return (
+		<div>
+			<h1>User Details</h1>
+			<p>
+				<strong>Name</strong>: {user.name}
+			</p>
+			<p>
+				<strong>Username</strong>: {user.username}
+			</p>
+			<p>
+				<strong>Email</strong>: {user.email}
+			</p>
+			<p>
+				<strong>Phone</strong>: {user.phone}
+			</p>
+			<p>
+				<strong>Website</strong>: {user.website}
+			</p>
+		</div>
+	)
+}
+
+export default About
